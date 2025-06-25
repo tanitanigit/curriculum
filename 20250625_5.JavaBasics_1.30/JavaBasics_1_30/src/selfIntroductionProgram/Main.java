8bf34ba1147224785a5989e9fa571f46e42d5efd
Tan_N <Tan_N@192.168.10.104>
2025-06-26 02:07:15 +0900

    Javaカリキュラム「1.30 自己紹介プログラムを作成①」の課題提出

---
+++ 20250625_5.JavaBasics_1.30/JavaBasics_1_30/src/selfIntroductionProgram/Main.java
@@ -0,0 +1,55 @@
+/*
+
+問題1：インスタンスフィールドを定義してください
+・フィールド名：name(データ型：string型)
+・フィールド名：age(データ型：int型)
+・フィールド名：height(データ型：double型)
+
+問題2：コンストラクタを定義してください（下記それぞれの引数）
+・name
+・age
+・height
+
+問題3：コンストラクタの中で各インスタンスフィールドに値をセットしてください
+
+問題4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+
+問題5：Main.javaの引数にweightの60を入れてください
+
+問題6：インスタンスメソッド「bmi」を定義してください（戻り値：double）
+
+問題7：bmiメソッドでインスタンスのBMIを返すようにしてください
+
+問題8：インスタンスメソッド「print」を定義してください（戻り値：void）
+
+問題9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+
+問題10：人数の合計を「合計○人です」と出力してください。
+
+ */
+
+package selfIntroductionProgram;
+
+// クラス「Main」を定義
+class Main {
+	// mainメソッド
+	public static void main(String[] args) {
+		// 問題5：Main.javaの引数にweightの60を入れてください
+		// Personクラスのインスタンス「person1」を作成し、各値を渡す(名前、年齢、身長、体重)
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60.0);
+		
+		// 指定の文字列を表示(Personクラスのフィールドから名前を取得)
+		System.out.println(person1.name);
+		// 指定の文字列を表示(Personクラスのフィールドから年齢を取得)
+		System.out.println(person1.age);
+		// 指定の文字列を表示(Personクラスのフィールドから身長を取得)
+		System.out.println(person1.height);
+
+		// person1のメソッド「print」を呼び出す(名前、年齢、BMIを表示)
+		person1.print();
+
+		// 問題10：人数の合計を「合計○人です」と出力してください。
+		// 指定の文字列を表示(静的メンバ変数を使って人数の合計を表示)
+		System.out.println("合計" + Person.count + "人です");
+	}
+}
---
+++ 20250625_5.JavaBasics_1.30/JavaBasics_1_30/src/selfIntroductionProgram/Person.java
@@ -0,0 +1,59 @@
+package selfIntroductionProgram;
+
+// クラスPersonを定義
+class Person {
+
+	// 問題1：インスタンスフィールドを定義してください
+	// ・フィールド名：name(データ型：string型)
+	// ・フィールド名：age(データ型：int型)
+	// ・フィールド名：height(データ型：double型)
+	// 文字列型の「name」をフィールドに宣言(名前)
+	String name;
+	// 整数型の「age」をフィールドに宣言(年齢)
+	int age;
+	// 倍精度浮動小数点数型の「height」をフィールドに宣言(身長)
+	double height;
+	
+	// 問題4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+	// 倍精度浮動小数点数型の「weight」をフィールドに宣言(体重)
+	double weight;
+	// 整数型の「count」を静的メンバ変数としてフィールドに宣言(全体の人数をカウントする)
+	static int count = 0;
+
+	// 問題2：コンストラクタを定義してください（下記それぞれの引数）
+	// name,age,height
+	// コンストラクタ「Person」を定義し、文字列型の「name」、整数型の「age」、
+	// 倍精度浮動小数点数型の「height」、「weight」の値を受け取る
+	Person(String name, int age, double height, double weight) {
+		
+		// 問題3：コンストラクタの中で各インスタンスフィールドに値をセットしてください		
+		// このクラスの変数を指す「this」を使い、各引数をフィールドに代入
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		// 問題4：インスタンスフィールド「weight」を定義し、コンストラクタの中で値をセットしてください（double型）
+		this.weight = weight;
+
+		// インクリメントで「count」の値を1増やす(インスタンスが1つ作られるたびに人数を1増やす)
+		count++;
+	}
+
+	// 問題6：インスタンスメソッド「bmi」を定義してください（戻り値：double）
+	// 浮動小数点数型のメソッド「bmi」を引数なしで定義
+	double bmi() {
+		// 問題7：bmiメソッドでインスタンスのBMIを返すようにしてください
+		// returnでBMI値を計算したものを戻り値として返す
+		// (フィールドからweight(体重)とheight(身長)の値を取得し計算する)
+		return this.weight / (this.height * this.height);
+	}
+
+	// 問題8：インスタンスメソッド「print」を定義してください（戻り値：void）
+	// 戻り値なしのメソッド「print」を引数なしで定義
+	void print() {
+		// 問題9：printメソッドの中でthisを用いて「名前は〇〇です」,「年は〇〇です」「BMIは○○です」と出力してください
+		// 指定の文字列を表示(名前、年齢、BMIの値を使った自己紹介文)
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age + "歳です");
+		System.out.println("BMIは" + this.bmi() + "です");
+	}
+}
